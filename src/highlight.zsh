0242c7eff18133558e4e1319347d8c44fb2435cc
Eric Freese <ericdfreese@gmail.com>
2016-02-24 13:59:49 -0700

    Fix highlighting when sh_word_split option is enabled

--- src/highlight.zsh
+++ src/highlight.zsh
@@ -15,7 +15,7 @@ _zsh_autosuggest_highlight_reset() {
 _zsh_autosuggest_highlight_apply() {
 	if [ $#POSTDISPLAY -gt 0 ]; then
 		_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT="$#BUFFER $(($#BUFFER + $#POSTDISPLAY)) $ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE"
-		region_highlight+=($_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT)
+		region_highlight+=("$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT")
 	else
 		unset _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT
 	fi
