f0a745576ff69fa608421ee7214d4cd77b43e62f
Eric Freese <ericdfreese@gmail.com>
2016-03-01 13:16:10 -0700

    bump version v0.2.17

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -1,6 +1,6 @@
 # Fish-like fast/unobtrusive autosuggestions for zsh.
 # https://github.com/zsh-users/zsh-autosuggestions
-# v0.2.16
+# v0.2.17
 # Copyright (c) 2013 Thiago de Arruda
 # Copyright (c) 2016 Eric Freese
 # 
