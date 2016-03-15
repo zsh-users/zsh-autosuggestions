dc822c54f8d66f9f6e411970480844e004ed9caa
Eric Freese <ericdfreese@gmail.com>
2016-03-14 21:50:04 -0600

    Fix #133

--- src/highlight.zsh
+++ src/highlight.zsh
@@ -5,6 +5,8 @@
 
 # If there was a highlight, remove it
 _zsh_autosuggest_highlight_reset() {
+	typeset -g _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT
+
 	if [ -n "$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT" ]; then
 		region_highlight=("${(@)region_highlight:#$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT}")
 		unset _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT
@@ -13,6 +15,8 @@ _zsh_autosuggest_highlight_reset() {
 
 # If there's a suggestion, highlight it
 _zsh_autosuggest_highlight_apply() {
+	typeset -g _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT
+
 	if [ $#POSTDISPLAY -gt 0 ]; then
 		_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT="$#BUFFER $(($#BUFFER + $#POSTDISPLAY)) $ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE"
 		region_highlight+=("$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT")
--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -196,6 +196,8 @@ _zsh_autosuggest_invoke_original_widget() {
 
 # If there was a highlight, remove it
 _zsh_autosuggest_highlight_reset() {
+	typeset -g _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT
+
 	if [ -n "$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT" ]; then
 		region_highlight=("${(@)region_highlight:#$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT}")
 		unset _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT
@@ -204,6 +206,8 @@ _zsh_autosuggest_highlight_reset() {
 
 # If there's a suggestion, highlight it
 _zsh_autosuggest_highlight_apply() {
+	typeset -g _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT
+
 	if [ $#POSTDISPLAY -gt 0 ]; then
 		_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT="$#BUFFER $(($#BUFFER + $#POSTDISPLAY)) $ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE"
 		region_highlight+=("$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT")
