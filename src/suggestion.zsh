7c2cb38f35e8576dcc05142b7a78eb1545b38b97
Eric Freese <ericdfreese@gmail.com>
2016-06-10 13:19:30 -0600

    Fix #168 and #130: Escape tildes when fetching suggestions

--- src/suggestion.zsh
+++ src/suggestion.zsh
@@ -17,5 +17,5 @@ _zsh_autosuggest_escape_command() {
 	setopt localoptions EXTENDED_GLOB
 
 	# Escape special chars in the string (requires EXTENDED_GLOB)
-	echo -E "${1//(#m)[\\()\[\]|*?]/\\$MATCH}"
+	echo -E "${1//(#m)[\\()\[\]|*?~]/\\$MATCH}"
 }
