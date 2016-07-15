9572ccbea2e8290981bc91d5d63b2d555fbafc55
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2016-07-15 09:39:33 +0100

    Disable autosuggest if buffer is too large

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -242,7 +242,7 @@ _zsh_autosuggest_modify() {
 
 	# Get a new suggestion if the buffer is not empty after modification
 	local suggestion
-	if [ $#BUFFER -gt 0 ]; then
+	if [ $#BUFFER -gt 0 -a $#BUFFER -lt 20 ]; then
 		suggestion="$(_zsh_autosuggest_suggestion "$BUFFER")"
 	fi
 
