e6591d5de05dc51848e6680d6a009d23610a888e
Eric Freese <ericdfreese@gmail.com>
2017-01-19 22:33:17 -0700

    Add RSpec for high-level integration testing

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'rspec'
+gem 'rspec-wait'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'rspec/wait'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.wait_timeout = 2
+end
