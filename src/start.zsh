4321fc097c408f6a8dfab8acf36d4706adb35386
Eric Freese <ericdfreese@gmail.com>
2017-02-17 23:20:04 -0700

    We need to bind on every precmd to ensure we wrap other wrappers
    Specifically, highlighting breaks if our widgets are wrapped by z-syn-h
    widgets.

--- src/start.zsh
+++ src/start.zsh
@@ -5,8 +5,6 @@
 
 # Start the autosuggestion widgets
 _zsh_autosuggest_start() {
-	add-zsh-hook -d precmd _zsh_autosuggest_start
-
 	_zsh_autosuggest_feature_detect
 	_zsh_autosuggest_bind_widgets
 
--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -609,8 +609,6 @@ _zsh_autosuggest_async_start() {
 
 # Start the autosuggestion widgets
 _zsh_autosuggest_start() {
-	add-zsh-hook -d precmd _zsh_autosuggest_start
-
 	_zsh_autosuggest_feature_detect
 	_zsh_autosuggest_bind_widgets
 
