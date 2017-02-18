e3fa4e4904fede78390152e9a1cc6f9f2ec9b1cb
Eric Freese <ericdfreese@gmail.com>
2017-02-18 10:47:53 -0700

    Don't do anything but re-bind widgets on each precmd
    There's no need to re-run feature detection or async_start on every
    precmd. Just do those once.

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -609,8 +609,16 @@ _zsh_autosuggest_async_start() {
 
 # Start the autosuggestion widgets
 _zsh_autosuggest_start() {
+	add-zsh-hook -d precmd _zsh_autosuggest_start
+
 	_zsh_autosuggest_feature_detect
 	_zsh_autosuggest_bind_widgets
+
+	# Re-bind widgets on every precmd to ensure we wrap other wrappers.
+	# Specifically, highlighting breaks if our widgets are wrapped by
+	# zsh-syntax-highlighting widgets. This also allows modifications
+	# to the widget list variables to take effect on the next precmd.
+	add-zsh-hook precmd _zsh_autosuggest_bind_widgets
 
 	if [ -n "${ZSH_AUTOSUGGEST_USE_ASYNC+x}" ]; then
 		_zsh_autosuggest_async_start
