4c88da24fa06a3219d1dbf023121a90336a687e1
Arnaud Venturi <arnaud.venturi@chauffeur-prive.com>
2018-05-07 13:35:40 +0200

    Cleanup

--- spec/integrations/vicmd_partial_accept_spec.rb
+++ spec/integrations/vicmd_partial_accept_spec.rb
@@ -2,12 +2,6 @@ describe 'a vicmd mode partial-accept widget' do
   let(:widget) { 'vi-forward-word-end' }
 
   context 'in vicmd mode' do
-#    -> do
-#      let(:before_sourcing) do
-#        session.
-#      end
-#    end
-
     it 'moves the cursor through suggestion as expected' do
       session.run_command("bindkey s vi-cmd-mode")
       with_history('foobar foo') do
