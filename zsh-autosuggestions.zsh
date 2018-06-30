dad6be4d5ec5c5446b7d67ada8b81ccd7074a9d4
Eric Freese <ericdfreese@gmail.com>
2018-06-30 15:04:56 -0600

    Remove unused feature detection
    Not needed after move away from zpty for async

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -123,25 +123,6 @@ _zsh_autosuggest_escape_command() {
 }
 
 #--------------------------------------------------------------------#
-# Feature Detection                                                  #
-#--------------------------------------------------------------------#
-
-_zsh_autosuggest_feature_detect_zpty_returns_fd() {
-	typeset -g _ZSH_AUTOSUGGEST_ZPTY_RETURNS_FD
-	typeset -h REPLY
-
-	zpty zsh_autosuggest_feature_detect '{ zshexit() { kill -KILL $$; sleep 1 } }'
-
-	if (( REPLY )); then
-		_ZSH_AUTOSUGGEST_ZPTY_RETURNS_FD=1
-	else
-		_ZSH_AUTOSUGGEST_ZPTY_RETURNS_FD=0
-	fi
-
-	zpty -d zsh_autosuggest_feature_detect
-}
-
-#--------------------------------------------------------------------#
 # Widget Helpers                                                     #
 #--------------------------------------------------------------------#
 
