f1c3b98774bb52667fe3303ace477898aedd3b9b
Eric Freese <ericdfreese@gmail.com>
2018-07-02 09:31:48 -0600

    Only capture completions at the end of the buffer.
    To prevent the suggestion from not starting with the buffer string.
    
    Example:
    
    `ls / /[cursor left][cursor left]b`
    
    Before the patch, suggests `ls /b /ls /bin/ /`
    
    After the patch, suggests `ls /b /bin/`.
    
    https://github.com/zsh-users/zsh-autosuggestions/issues/343#issuecomment-401675712

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -487,6 +487,9 @@ _zsh_autosuggest_capture_completion_widget() {
 	local -a +h comppostfuncs
 	comppostfuncs=(_zsh_autosuggest_capture_postcompletion)
 
+	# Only capture completions at the end of the buffer
+	CURSOR=$#BUFFER
+
 	# Run the original widget wrapping `.complete-word` so we don't
 	# recursively try to fetch suggestions, since our pty is forked
 	# after autosuggestions is initialized.
