286656635c6b9d7a1e968fa61e60522e423e5387
Eric Freese <ericdfreese@gmail.com>
2018-11-24 09:44:46 -0700

    v0.5.0

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -1,6 +1,6 @@
 # Fish-like fast/unobtrusive autosuggestions for zsh.
 # https://github.com/zsh-users/zsh-autosuggestions
-# v0.4.3
+# v0.5.0
 # Copyright (c) 2013 Thiago de Arruda
 # Copyright (c) 2016-2018 Eric Freese
 # 
