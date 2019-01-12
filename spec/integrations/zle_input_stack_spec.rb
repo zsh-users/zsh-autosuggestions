de4256a63b26ff50b5741d5fbf1cec67b38963c7
Eric Freese <ericdfreese@gmail.com>
2019-01-11 18:58:05 -0700

    Enable zle -U test in newer zsh versions

--- spec/integrations/zle_input_stack_spec.rb
+++ spec/integrations/zle_input_stack_spec.rb
@@ -9,8 +9,9 @@ describe 'using `zle -U`' do
 
   let(:options) { ['unset ZSH_AUTOSUGGEST_USE_ASYNC', 'ZSH_AUTOSUGGEST_STRATEGY=test'] }
 
-  # TODO: This is only possible with the $KEYS_QUEUED_COUNT widget parameter, coming soon...
-  xit 'does not fetch a suggestion for every inserted character' do
+  it 'does not fetch a suggestion for every inserted character' do
+    skip 'KEYS_QUEUED_COUNT not supported below v5.4.0' if session.zsh_version < Gem::Version.new('5.4.0')
+
     session.send_keys('C-b')
     wait_for { session.content }.to eq('echo hello')
   end
