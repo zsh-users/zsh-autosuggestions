a5dc4a8db4a5e92e0a18d97e0f78c1aa8696fad7
Eric Freese <ericdfreese@gmail.com>
2019-04-09 14:43:48 -0600

    Fix version in compiled plugin script

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -1,6 +1,6 @@
 # Fish-like fast/unobtrusive autosuggestions for zsh.
 # https://github.com/zsh-users/zsh-autosuggestions
-# v0.5.0
+# v0.5.1
 # Copyright (c) 2013 Thiago de Arruda
 # Copyright (c) 2016-2018 Eric Freese
 # 
