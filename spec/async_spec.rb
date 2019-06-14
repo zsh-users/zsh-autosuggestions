527fd17ffc9ec0343e6d7718ff1ff245c703756f
Henré Botha <henrebotha@gmail.com>
2019-06-14 17:48:31 +0200

    Don't fetch suggestions after copy-earlier-word
    Like {up,down}-line-or-beginning-search, this widget relies on
    `$LASTWIDGET` being set to function correctly on subsequent invocations.
    
    When asynchronous suggestions are enabled, and the widget triggers a
    suggestion to be fetched, `autosuggest-suggest` will be called and
    $LASTWIDGET will be set to it.

--- spec/async_spec.rb
+++ spec/async_spec.rb
@@ -27,6 +27,29 @@ context 'with asynchronous suggestions enabled' do
     end
   end
 
+  describe '`copy-earlier-word`' do
+    let(:before_sourcing) do
+      -> do
+        session.
+          run_command('autoload -Uz copy-earlier-word').
+          run_command('zle -N copy-earlier-word').
+          run_command('bindkey "^N" copy-earlier-word')
+      end
+    end
+
+    it 'should cycle through previous words in the buffer' do
+      session.clear_screen
+      session.send_string('foo bar baz')
+      sleep 0.5
+      session.send_keys('C-n')
+      wait_for { session.content }.to eq('foo bar bazbaz')
+      session.send_keys('C-n')
+      wait_for { session.content }.to eq('foo bar bazbar')
+      session.send_keys('C-n')
+      wait_for { session.content }.to eq('foo bar bazfoo')
+    end
+  end
+
   describe 'pressing ^C after fetching a suggestion' do
     before do
       skip 'Workaround does not work below v5.0.8' if session.zsh_version < Gem::Version.new('5.0.8')
