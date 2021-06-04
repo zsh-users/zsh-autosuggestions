fcca87555fe5c0c93c5ba865505a7e5d7a1a95cb
Eric Freese <ericdfreese@gmail.com>
2021-06-04 16:00:37 -0600

    v0.7.0

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -1,6 +1,6 @@
 # Fish-like fast/unobtrusive autosuggestions for zsh.
 # https://github.com/zsh-users/zsh-autosuggestions
-# v0.6.4
+# v0.7.0
 # Copyright (c) 2013 Thiago de Arruda
 # Copyright (c) 2016-2021 Eric Freese
 # 
