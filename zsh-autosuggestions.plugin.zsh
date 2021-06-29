844304b2cd12b5ee7e2db9987d4d9b9d2b3500d5
JT <jeffreytse.mail@gmail.com>
2021-06-29 17:04:04 +0800

    feat: standardized handling `$0`
    For all kinds of plugin managers to get the plugin’s location properly, we need to handle `$0` correctly.

--- zsh-autosuggestions.plugin.zsh
+++ zsh-autosuggestions.plugin.zsh
@@ -1,1 +1,6 @@
-source ${0:A:h}/zsh-autosuggestions.zsh
+# According to the standard:
+# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+source ${0:h}/zsh-autosuggestions.zsh
