33f442af079e789388abbc3267ae560435175f04
Maciej Dems <maciej.dems@p.lodz.pl>
2023-06-09 11:20:34 +0200

    Disable async

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -3,7 +3,7 @@
 # v0.7.0
 # Copyright (c) 2013 Thiago de Arruda
 # Copyright (c) 2016-2021 Eric Freese
-# 
+#
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
 # files (the "Software"), to deal in the Software without
@@ -12,10 +12,10 @@
 # copies of the Software, and to permit persons to whom the
 # Software is furnished to do so, subject to the following
 # conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 # OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -893,9 +893,9 @@ autoload -Uz add-zsh-hook is-at-least
 # older versions because there is a bug when using async mode where ^C does not
 # work immediately after fetching a suggestion.
 # See https://github.com/zsh-users/zsh-autosuggestions/issues/364
-if is-at-least 5.0.8; then
-	typeset -g ZSH_AUTOSUGGEST_USE_ASYNC=
-fi
+# if is-at-least 5.0.8; then
+# 	typeset -g ZSH_AUTOSUGGEST_USE_ASYNC=
+# fi
 
 # Start the autosuggestion widgets on the next precmd
 add-zsh-hook precmd _zsh_autosuggest_start
